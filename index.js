911f2cef1fab7c7f4d9e7c8dd19177de3061a3b0
Igor <krytsun@gmail.com>
2023-03-02 15:34:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,23 @@
 //console.log("2" * true, "2" * 2, 2 * true);
 
 //console.log("3" / true, "3" / 2, 3 * true);
+
 /* let bool = true;
-let num = Number(bool); */
+let num = Number(bool); 
+console.log(num) */
+
 /* let str = "4";
-let num = Number(str); */
+let num = Number(str); 
+console.log(num) */
+
 /* let num = 45;
-let str = String(num);
- */
+let str = String(num); 
+console.log(str) */
 
 /* let str = "hello";
-let bool = Boolean(str); */
+let bool = Boolean(str); 
+console.log(bool) */
+
 /* let num = 0;
 let bool = Boolean(num); 
 console.log(bool) */
