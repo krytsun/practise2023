de64bc651d47a0fa0032d71beed4ac3e06431df2
Igor <krytsun@gmail.com>
2023-03-02 15:24:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//console.log("1" + true, "1" + 2, 1 + true);
+
+//console.log("2" * true, "2" * 2, 2 * true);
+
+//console.log("3" / true, "3" / 2, 3 * true);
+/* let bool = true;
+let num = Number(bool); */
+/* let str = "4";
+let num = Number(str); */
+/* let num = 45;
+let str = String(num);
+ */
+
+/* let str = "hello";
+let bool = Boolean(str); */
+/* let num = 0;
+let bool = Boolean(num); 
+console.log(bool) */
